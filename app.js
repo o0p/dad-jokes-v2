6750b88493e245df347001ff8eefb9046d19b99e
Russell <russelldev@protonmail.com>
2021-01-03 16:03:05 +0000

    Add joke generator fetch

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const jokEl = document.getElementById('joke');
+const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
+generateJoke();
+
+function generateJoke() {
+    const config = {
+        headers: {
+            'Accept':'application/json'
+        }
+    }
+    fetch('https://icanhazdadjoke.com/', config)
+    .then(res => res.json())
+    .then(data => {
+        jokeEl.innerHTML = data.joke;
+    });
+}
